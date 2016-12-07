6b7bd9bde51b058a660013ab6a85d6a0620f8ae8
Sherwoodwt <tsherwood@2ndwatch.com>
2016-12-07 00:52:36 -0800

    added wiring to RDS instance BandManagerDatabase and did a default migration.

--- bandsite/bandsite/settings.py
+++ bandsite/bandsite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bandsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BandManagerDatabase',
+        'HOST': 'bandmanagerdatabase.cgpcwklvtbcv.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'USER': 'jaun_chunk',
+        'PASSWORD': 'Bilbo55%',
     }
 }
 
