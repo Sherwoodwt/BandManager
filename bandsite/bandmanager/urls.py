bd38fcf4f891dbed04bff110295823b0f5eec8f4
Willy Pluto <Willy Pluto>
2017-01-02 15:19:02 -0800

    Added Unassigned Member to band, could be programmatically enforced, but other options to make a relation between tasks and members optional should be looked into. Some tasks will not be assigned to anybody until somebody assigns them (like github issues).

--- bandsite/bandmanager/urls.py
+++ bandsite/bandmanager/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<band_id>[0-9]+)/$', views.band, name='band'),
-	
+	url(r'^(?P<band_id>[0-9]+)/tasklist/$', views.tasklist, name='tasklist'),
 ]
--- bandsite/bandmanager/views.py
+++ bandsite/bandmanager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Band
 
@@ -10,7 +10,13 @@ def index(request):
 	return render(request, "index.html")
 
 def band(request, band_id):
-	band = Band.objects.get(id=band_id)
+	band = get_object_or_404(Band, pk=band_id)
 	members = band.member_set.all()
 	context = {"band": band, "members": members}
 	return render(request, "band.html", context)
+
+def tasklist(request, band_id):
+	band = get_object_or_404(Band, pk=band_id)
+	tasks = band.task_set.all()
+	context = {"band": band, "tasks": tasks}
+	return render(request, "tasklist.html", context)
