0c9b3cb6f056b9d469e0a1ec9422d3ff15c99cfb
Willy Pluto <Willy Pluto>
2017-01-06 00:55:14 -0800

    Adding new tasks now works. NewTask page is done for now.

--- bandsite/bandmanager/urls.py
+++ bandsite/bandmanager/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<band_id>[0-9]+)/$', views.band, name='band'),
 	url(r'^(?P<band_id>[0-9]+)/tasklist/$', views.tasklist, name='tasklist'),
+	url(r'^(?P<band_id>[0-9]+)/tasklist/newtask/$', views.newtask, name='newtask'),
+	url(r'^(?P<band_id>[0-9]+)/tasklist/addtask/$', views.addtask, name='addtask'),
 ]
--- bandsite/bandmanager/views.py
+++ bandsite/bandmanager/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from django.shortcuts import render, get_object_or_404
 
-from .models import Band
+from .models import Band, Task, Member
+
 
 # Create your views here.
 def index(request):
 	return render(request, "index.html")
+
 
 def band(request, band_id):
 	band = get_object_or_404(Band, pk=band_id)
@@ -15,8 +18,28 @@ def band(request, band_id):
 	context = {"band": band, "members": members}
 	return render(request, "band.html", context)
 
+
 def tasklist(request, band_id):
 	band = get_object_or_404(Band, pk=band_id)
 	tasks = band.task_set.all()
 	context = {"band": band, "tasks": tasks}
 	return render(request, "tasklist.html", context)
+
+
+def newtask(request, band_id):
+	band = get_object_or_404(Band, pk=band_id)
+	members = band.member_set.all()
+	context = {"band": band, "members": members}
+	return render(request, "newtask.html", context)
+
+
+#handles adding new tasks from newtask page
+def addtask(request, band_id):
+	title = request.POST['TaskTitle']
+	description = request.POST['TaskDescription']
+	difficulty = request.POST['TaskDifficulty']
+	priority = request.POST['TaskPriority']
+	member = request.POST['TaskAssignee']
+	newTask = Task(band=Band.objects.get(pk=band_id), member=Member.objects.get(pk=member), title=title, description=description, difficulty=difficulty, priority=priority)
+	newTask.save()
+	return HttpResponseRedirect(reverse('tasklist', args=band_id))
