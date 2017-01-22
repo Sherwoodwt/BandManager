ed931f9e7e47f9d0e37585c28f34fa5be55f60c5
Willy Pluto <Willy Pluto>
2017-01-21 17:55:42 -0800

    added more fields to form to create member at the same time.

--- bandsite/bandsite/forms.py
+++ bandsite/bandsite/forms.py
@@ -1,13 +1,24 @@
 # Used for user signup form to extend UserCreationForm functionality but add email and first/lastname
+# and create member object
 
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from bandmanager.models import Member, Band
+
 class SignupForm(UserCreationForm):
+    
+    def getBands():
+        bandList = []
+        for eachBand in Band.objects.all():
+            bandList.append((eachBand.pk, eachBand.name))
+        return bandList
+        
     email = forms.EmailField(label='Email', required=True)
     firstName = forms.CharField(label='First Name', max_length=30)
     lastName = forms.CharField(label='Last Name', max_length=30)
+    band = forms.ChoiceField(widget=forms.Select, choices=getBands())
 
     class Meta:
         model = User
@@ -20,4 +31,10 @@ class SignupForm(UserCreationForm):
         user.last_name = self.cleaned_data["lastName"]
         if commit:
             user.save()
+        # Create Member
+        newBand = self.cleaned_data["band"]
+        username = user.username
+        pic = self.cleaned_data["picture"]
+        member = Member(band=newBand, name=username, user=user, picture_url=pic, contact_info=user.email)
+        member.save()
         return user
